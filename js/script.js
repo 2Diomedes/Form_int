fe1c18658e161b369148a43ddc25094328fa2034
Caroline <c.bove16@gmail.com>
2018-07-02 12:12:07 +0200

    color verification done

--- js/script.js
+++ js/script.js
@@ -1,28 +1,44 @@
 $(document).ready(function() {
 
-  // bouton send
-  $('#send').click(function() {
+    var allInputs = $('.text');
+    // bouton send
+    $('#send').click(function() {
 
-    if ($('.text').val('')) {
-      console.log($('.text').val(''));
-      alert('Inputs are empty');
-    } else if ($('.text').val().length < 5) {
-      $('.text').val('');
-      alert('It\'s too short. It must content at least 5 characters.');
-    } else if (password != verifyPassword) {
-      $('#password, #verifyPassword').val('');
-      alert('Your password is different than your verifyPassword');
-    }
+        $(allInputs).each(function(i) {
+            if ($(this).val() == "") {
+                alert('One or more inputs are empty');
+                $(this).css({
+                    'border-color': 'red',
+                })
+            } else if ($(this).val().length < 5) {
+                alert('It\'s too short. It must content at least 5 characters.');
+                $(this).css({
+                    'border-color': 'red',
+                });
+            } else if ($('#password').val() != $('#verifyPassword').val()) {
+                alert('Your password is different than your verifyPassword');
+                $(this).css({
+                    'border-color': 'red',
+                });
+            } else {
 
-  });
-  // bouton send
+                $(this).css({
+                    'border-color': 'green',
+                });
 
 
-  // bouton reset
-  $('#reset').click(function() {
-    $('.text').val('');
-  });
-  // bouton reset
+            }
+        });
+
+    });
+    // bouton send
+
+
+    // bouton reset
+    $('#reset').click(function() {
+        $('.text').val('');
+    });
+    // bouton reset
 
 
 });
