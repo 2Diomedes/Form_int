b3b3151749609bbba4e6c9e90232facaf4186e8e
2Diomedes <t.martinteyssier@gmail.com>
2018-07-03 10:23:00 +0200

    CSS done

--- js/script.js
+++ js/script.js
@@ -1,44 +1,50 @@
 $(document).ready(function() {
 
-    var allInputs = $('.text');
-    // bouton send
-    $('#send').click(function() {
+  var allInputs = $('.text');
+  // bouton send
+  $('#send').click(function() {
 
-        $(allInputs).each(function(i) {
-            if ($(this).val() == "") {
-                alert('One or more inputs are empty');
-                $(this).css({
-                    'border-color': 'red',
-                })
-            } else if ($(this).val().length < 5) {
-                alert('It\'s too short. It must content at least 5 characters.');
-                $(this).css({
-                    'border-color': 'red',
-                });
-            } else if ($('#password').val() != $('#verifyPassword').val()) {
-                alert('Your password is different than your verifyPassword');
-                $(this).css({
-                    'border-color': 'red',
-                });
-            } else {
+    console.log($('#pseudo').val().length);
 
-                $(this).css({
-                    'border-color': 'green',
-                });
+    if ($('#pseudo').val().length > 12) {
+      alert('Your pseudo is too long! It must contain a maximum of 12 characters.');
+    } else if ($('input').val() == "") {
+      alert('An input is empty');
+    } else if ($('#password').val() != $('#verifyPassword').val()) {
+      alert('Your password is different than your verifyPassword');
+    } else {}
 
+    $("#validate").bind("click", validate);
 
-            }
+    $(allInputs).each(function(i) {
+      if ($(this).val() == "") {
+        $(this).css({
+          'border-color': 'red',
+        })
+      } else if ($(this).val().length < 5) {
+        alert('It\'s too short. It must content at least 5 characters.');
+        $(this).css({
+          'border-color': 'red',
         });
-
+      } else if ($('#password').val() != $('#verifyPassword').val()) {
+        $(this).css({
+          'border-color': 'red',
+        });
+      } else {
+        $(this).css({
+          'border-color': 'green',
+        });
+      }
     });
-    // bouton send
+  });
+  // bouton send
 
 
-    // bouton reset
-    $('#reset').click(function() {
-        $('.text').val('');
-    });
-    // bouton reset
+  // bouton reset
+  $('#reset').click(function() {
+    $('.text').val('');
+  });
+  // bouton reset
 
 
 });
