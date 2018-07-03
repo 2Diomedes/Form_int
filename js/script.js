e03866084a1314dc92b05d33100885cf1124a985
Caroline <c.bove16@gmail.com>
2018-07-03 11:49:20 +0200

    restart

--- js/script.js
+++ js/script.js
@@ -1,50 +1,109 @@
-$(document).ready(function() {
-
-  var allInputs = $('.text');
-  // bouton send
-  $('#send').click(function() {
-
-    console.log($('#pseudo').val().length);
-
-    if ($('#pseudo').val().length > 12) {
-      alert('Your pseudo is too long! It must contain a maximum of 12 characters.');
-    } else if ($('input').val() == "") {
-      alert('An input is empty');
-    } else if ($('#password').val() != $('#verifyPassword').val()) {
-      alert('Your password is different than your verifyPassword');
-    } else {}
-
-    $("#validate").bind("click", validate);
-
-    $(allInputs).each(function(i) {
-      if ($(this).val() == "") {
-        $(this).css({
-          'border-color': 'red',
-        })
-      } else if ($(this).val().length < 5) {
-        alert('It\'s too short. It must content at least 5 characters.');
-        $(this).css({
-          'border-color': 'red',
-        });
-      } else if ($('#password').val() != $('#verifyPassword').val()) {
-        $(this).css({
-          'border-color': 'red',
-        });
-      } else {
-        $(this).css({
-          'border-color': 'green',
-        });
-      }
-    });
-  });
-  // bouton send
-
-
-  // bouton reset
-  $('#reset').click(function() {
-    $('.text').val('');
-  });
-  // bouton reset
-
+$(document).ready(function(){
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//
+//     var allInputs = $('.text');
+//
+//     $('#send').click(function() {
+//
+//
+//         if ($('#pseudo').val().length > 12) {
+//             alert('Your pseudo is too long! It must contain a maximum of 12 characters.');
+//         } else if ($('input').val() == "") {
+//             alert('An input is empty');
+//         } else if ($('#password').val() != $('#verifyPassword').val()) {
+//             alert('Your password is different than your verifyPassword');
+//         } else {}
+//
+//
+//
+//         $(allInputs).each(function(i) {
+//             if ($(this).val() == "") {
+//                 $(this).css({
+//                     'border-color': 'red',
+//                 })
+//             } else if ($(this).val().length < 5) {
+//                 alert('It\'s too short. It must content at least 5 characters.');
+//                 $(this).css({
+//                     'border-color': 'red',
+//                 });
+//             } else if ($('#password').val() != $('#verifyPassword').val()) {
+//                 $(this).css({
+//                     'border-color': 'red',
+//                 });
+//             } else {
+//                 $(this).css({
+//                     'border-color': 'green',
+//                 });
+//             }
+//         });
+//     });
+//
+//     // $('#send').click(function() {
+//     //     var passwordFitness = 0;
+//     //     passwordFitness = passwordFitness + ($('#password').val().length);
+//     //     var passwordTab = ($('#password').val());
+//     //
+//     //     var passDico = [""];
+//     //     for (var i = 0; i < passwordTab.length; i++) {
+//     //         console.log('touri' + [i]);
+//     //         for (var j = 0; j < passDico.length; j++) {
+//     //             console.log('tourj' + [j]);
+//     //             if (passwordTab[i] == passDico[j]) {
+//     //             } else {
+//     //                 passDico.push(passwordTab[i]);
+//     //             }
+//     //         }
+//     //     }
+//     //
+//     // })
+//
+//
+//
+//
+//     $('#reset').click(function() {
+//         $('.text').val('');
+//     });
+//
+//
+//
+// });
